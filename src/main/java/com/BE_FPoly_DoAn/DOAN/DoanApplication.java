73d6204bb04efdf1a4c6873da860d59aac8f17df
dai phan phuc <dai582005@gmail.com>
2025-05-22 13:24:57 +0700

    init source

---
+++ src/main/java/com/BE_FPoly_DoAn/DOAN/DoanApplication.java
@@ -0,0 +1,12 @@
+package com.BE_FPoly_DoAn.DOAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoanApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DoanApplication.class, args);
+	}
+
+}
