e06acc3066b4db868fc94ba93bb4a7f2ae22b640
TriCargo <phunghoanglua2005@gmail.com>
2025-08-15 01:43:13 +0700

    temp work from master

--- src/main/java/com/BE_FPoly_DoAn/DOAN/Service/Impl/LichLamViecBacSiServiceImpl.java
+++ src/main/java/com/BE_FPoly_DoAn/DOAN/Service/Impl/LichLamViecBacSiServiceImpl.java
@@ -1,18 +1,25 @@
 package com.BE_FPoly_DoAn.DOAN.Service.Impl;
 
+import com.BE_FPoly_DoAn.DOAN.DTO.BacSi.GioKhamChiTietDto;
 import com.BE_FPoly_DoAn.DOAN.DTO.BacSi.LichLamViecDTO;
+import com.BE_FPoly_DoAn.DOAN.DTO.BacSi.LichLamViecHomNayChiTietDTO;
 import com.BE_FPoly_DoAn.DOAN.DTO.BacSi.LichLamViecResponseDTO;
 import com.BE_FPoly_DoAn.DOAN.Dao.BacSiRepository;
+import com.BE_FPoly_DoAn.DOAN.Dao.GioKhamChiTietRepository;
 import com.BE_FPoly_DoAn.DOAN.Dao.LichLamViecBacSiRepository;
 import com.BE_FPoly_DoAn.DOAN.Entity.BacSi;
 import com.BE_FPoly_DoAn.DOAN.Entity.LichLamViecBacSi;
+import com.BE_FPoly_DoAn.DOAN.Mapper.GioKhamChiTietMapper;
 import com.BE_FPoly_DoAn.DOAN.Mapper.LichLamViecMapper;
 import com.BE_FPoly_DoAn.DOAN.Response.NotificationCode;
 import com.BE_FPoly_DoAn.DOAN.Response.ServiceResponse;
 import com.BE_FPoly_DoAn.DOAN.Service.InterfaceService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -20,10 +27,12 @@ public class LichLamViecBacSiServiceImpl implements InterfaceService<LichLamViec
 
     private final LichLamViecBacSiRepository repository;
     private final BacSiRepository bacSiRepository;
+    private final GioKhamChiTietRepository repositoryGioKham;
 
-    public LichLamViecBacSiServiceImpl(LichLamViecBacSiRepository repository, BacSiRepository bacSiRepository) {
+    public LichLamViecBacSiServiceImpl(LichLamViecBacSiRepository repository, BacSiRepository bacSiRepository, GioKhamChiTietRepository repositoryGioKham) {
         this.repository = repository;
         this.bacSiRepository = bacSiRepository;
+        this.repositoryGioKham = repositoryGioKham;
     }
 
     @Override
@@ -92,20 +101,6 @@ public class LichLamViecBacSiServiceImpl implements InterfaceService<LichLamViec
         }
     }
 
-    public ServiceResponse<?> xoaLichLamViec(Integer lichId) {
-        Optional<LichLamViecBacSi> optional = repository.findById(lichId);
-        if (optional.isEmpty()) {
-            return ServiceResponse.error(NotificationCode.SERVICE_NOT_FOUND);
-        }
-
-        try {
-            repository.delete(optional.get());
-            return ServiceResponse.success(NotificationCode.SERVICE_DELETE_SUCCESS);
-        } catch (Exception e) {
-            return ServiceResponse.error(NotificationCode.SERVICE_DELETE_FAIL);
-        }
-    }
-
     public ServiceResponse<?> taoLichLamViec(Integer bacSiId, LichLamViecDTO dto) {
         try {
             boolean isExist = repository.existsByBacSi_BacSiIdAndNgayAndCaTruc(bacSiId, dto.getNgay(), dto.getCaTruc());
@@ -136,5 +131,69 @@ public class LichLamViecBacSiServiceImpl implements InterfaceService<LichLamViec
         } catch (Exception e) {
             return ServiceResponse.error(NotificationCode.WORK_SCHEDULE_CREATE_FAIL, e.getMessage());
         }
+    }
+
+    public ServiceResponse<?> getAllLichLamViec() {
+        try {
+            List<LichLamViecResponseDTO> list = repository.findAll()
+                    .stream()
+                    .map(LichLamViecMapper::toDto)
+                    .toList();
+            return ServiceResponse.success(NotificationCode.SERVICE_LIST, list);
+        } catch (Exception e) {
+            return ServiceResponse.error(NotificationCode.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    public ServiceResponse<?> getLichLamViecHomNay() {
+        try {
+            LocalDate today = LocalDate.now();
+            List<LichLamViecResponseDTO> list = repository.findByNgay(today)
+                    .stream()
+                    .map(LichLamViecMapper::toDto)
+                    .toList();
+            return ServiceResponse.success(NotificationCode.SERVICE_LIST, list);
+        } catch (Exception e) {
+            return ServiceResponse.error(NotificationCode.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    public ServiceResponse<?> getLichLamViecHomNayChiTiet() {
+        try {
+            LocalDate today = LocalDate.now();
+
+            List<LichLamViecBacSi> lichHomNay = repository.findByNgay(today);
+
+            List<LichLamViecHomNayChiTietDTO> ketQua = lichHomNay.stream()
+                    .map(lich -> LichLamViecHomNayChiTietDTO.builder()
+                            .bacSiId(lich.getBacSi().getBacSiId())
+                            .hoTen(lich.getBacSi().getNguoiDung().getHoTen())
+                            .ngay(lich.getNgay())
+                            .caTruc(lich.getCaTruc())
+                            .lyDoNghi(lich.getLyDoNghi())
+                            .gioKhamChiTiet(
+                                    repositoryGioKham.findByLichLamViecBacSi_LichlvId(lich.getLichlvId())
+                                            .stream()
+                                            .map(GioKhamChiTietMapper::toDto)
+                                            .toList()
+                            )
+                            .build()
+                    ).toList();
+
+            return ServiceResponse.success(NotificationCode.SERVICE_LIST, ketQua);
+
+        } catch (Exception e) {
+            return ServiceResponse.error(NotificationCode.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    public ServiceResponse<?> getLichLamViecTheoNgay(LocalDate ngay) {
+        return ServiceResponse.success(
+                NotificationCode.SERVICE_LIST,
+                repository.findByNgay(ngay)
+                        .stream()
+                        .map(LichLamViecMapper::toDto)
+                        .toList()
+        );
     }
 }
